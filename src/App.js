cd12164cfb5f4f8235e5a0b5d840582bfb51e732
RAJNIKANT1021 <113264449+RAJNIKANT1021@users.noreply.github.com>
2023-03-10 04:32:07 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './App.css'
+import Navbar from "./Components/Navbar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+  <div >
+    <Navbar/>
+    <div className="main d-flex flex-row">
+      <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
 
+    </div>
+    <div className="main d-flex flex-row">
+      <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+
+    </div>
+    <div className="main d-flex flex-row">
+      <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+
+    </div>
+    <div className="main d-flex flex-row">
+      <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+        <div className="hey">
+        </div> 
+
+    </div>
+  </div>)
+}
 export default App;
