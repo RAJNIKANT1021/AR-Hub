c86554106d594b9753fc7e2c01bb1a7906b7e629
RAJNIKANT1021 <113264449+RAJNIKANT1021@users.noreply.github.com>
2023-03-14 19:01:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React ,{useContext,useState}from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar";
 import Login from "./userauth/login";
@@ -6,54 +5,35 @@ import { BrowserRouter, Routes, Route,useLocation } from "react-router-dom";
 import Movies from "./Components/Movies";
 import Home from "./Components/Home";
 import Games from "./Components/Games";
-import Feed from "./Components/Feed";
-import Chat from "./Components/Chat";
-import Books from "./Components/Books";
-import LoginContext from './Context/LoginContext';
-import ToShow from './Components/ToShow';
-
-
 function App() { 
-
   const location = useLocation();
   const [loggedin, setloggedin] = useState(false);
   const[uid,setuid]=useState(null);
-
   function checker(id,uid){
     setloggedin(id);
     setuid(uid);
-
-
-
-  }
-
-
   return(
     
    <>
-   {/* jgh */}
     <Navbar loggedin={loggedin} checker={checker} />
     <Routes>  
-      <Route path="/" element={<Login checker={checker}/>}/>
-      {loggedin && <Route path="home" element={<Home/>} />}
-      {loggedin && <Route path="movies" element={<Movies/>}/>}
-      {loggedin && <Route path="books" element={<Books/>}/> }
-      {loggedin &&<Route path="books" element={<Books/>}/> }
-      {loggedin && <Route path="games" element={<Games/>}/>}
-      {loggedin && <Route path="feed" element={<Feed/>}/>}
-      {!loggedin && <Route path="chat" element={<Chat/>}/> }
-      {!loggedin && <Route path="home" element={<Home/>} />}
-      {!loggedin && <Route path="movies" element={<ToShow/>}/>}
-      {!loggedin && <Route path="books" element={<ToShow/>}/> }
-      {!loggedin &&<Route path="books" element={<ToShow/>}/> }
-      {!loggedin && <Route path="games" element={<ToShow/>}/>}
-      {!loggedin && <Route path="feed" element={<ToShow/>}/>}
-      {!loggedin && <Route path="chat" element={<ToShow/>}/> }
-
+      <Route exact path="/" element={<Login checker={checker} key={location.key}/>}/>
+      {loggedin && <Route  exact path="home" element={<Home key={location.key}/>}   />}
+      {loggedin && <Route exact path="movies" element={<Movies key={location.key}/>}  />}
+      {loggedin && <Route exact path="books" element={<Books key={location.key}/>} /> }
+      {loggedin &&<Route exact path="books" element={<Books key={location.key}/>}/> }
+      {loggedin && <Route exact path="games" element={<Games key={location.key}/>}  />}
+      {loggedin && <Route exact path="feed" element={<Feed key={location.key}/>}  />}
+      {loggedin && <Route exact path="chat" element={<Chat uid={uid}  key={location.key}/>}/> }
+      {!loggedin && <Route exact path="home" element={<Home/>} />}
+      {!loggedin && <Route exact path="movies" element={<ToShow/>}/>}
+      {!loggedin && <Route exact path="books" element={<ToShow/>}/> }
+      {!loggedin &&<Route exact path="books" element={<ToShow/>}/> }
+      {!loggedin && <Route exact path="games" element={<ToShow/>}/>}
+      {!loggedin && <Route exact path="feed" element={<ToShow/>}/>}
+      {!loggedin && <Route exact path="chat" element={<ToShow/>}/> }
       
         
-      
-       
      
       
     </Routes>
