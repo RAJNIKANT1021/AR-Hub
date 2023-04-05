abdad3f9fb7d0ba04c1caa22b26369255cd4a109
RAJNIKANT1021 <113264449+RAJNIKANT1021@users.noreply.github.com>
2023-04-06 03:41:44 +0530

    chg

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,9 +26,8 @@ function Navbar({loggedin,checker,uid}) {
   
   
   return (   
- <nav className="flex navbar navbar-expand-lg navbar-dark navbar-default" style={{position:'sticky'
- }}>
-  <Link  className="navbar-brand" to="#">A2R HUB</Link >
+<nav className="flex navbar navbar-expand-lg navbar-dark navbar-default" style={{position:'sticky'}}>
+  <Link className="navbar-brand" to="#">A2R HUB</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -36,70 +35,49 @@ function Navbar({loggedin,checker,uid}) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <Link  className="nav-link" to="/home">Home <span className="sr-only">(current)</span></Link >
+        <Link className="nav-link" to="/home" data-toggle="collapse" data-target=".navbar-collapse.show">Home</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/movies">Movies </Link >
+        <Link className="nav-link" to="/movies" data-toggle="collapse" data-target=".navbar-collapse.show">Movies</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/books">Books</Link >
+        <Link className="nav-link" to="/books" data-toggle="collapse" data-target=".navbar-collapse.show">Books</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/games">Games </Link >
+        <Link className="nav-link" to="/games" data-toggle="collapse" data-target=".navbar-collapse.show">Games</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/feed">Feed</Link >
+        <Link className="nav-link" to="/feed" data-toggle="collapse" data-target=".navbar-collapse.show">Feed</Link>
       </li>
-    
       <li className="nav-item">
-    
-     
-        <Link  className="nav-link" to="/chat">   <Badge badgeContent={4} color="secondary"> Chat    </Badge> </Link >
-      
-      
+        <Link className="nav-link" to="/chat" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <Badge badgeContent={4} color="secondary">Chat</Badge>
+        </Link>
       </li>
-        
-    
       <li className="nav-item dropdown">
-        <Link  className="nav-link dropdown-toggle" to="hello" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <Link className="nav-link dropdown-toggle" to="hello" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-toggle="collapse" data-target=".navbar-collapse.show">
           Dropdown
-        </Link >
+        </Link>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <Link  className="dropdown-item" to="/">Login</Link >
-          <Link  className="dropdown-item" to="#">Another action</Link >
+          <Link className="dropdown-item" to="/">Login</Link>
+          <Link className="dropdown-item" to="#">Another action</Link>
           <div className="dropdown-divider"></div>
-          <Link  className="dropdown-item" to="#">Something else here</Link >
+          <Link className="dropdown-item" to="#">Something else here</Link>
         </div>
       </li>
-      
     </ul>
     <div className="form-inline my-2 my-lg-0">
       <input className="searchbar form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-primary my-2 my-sm-0 " style={{color:"white"}}type="submit">Search</button>
+      <button className="btn btn-primary my-2 my-sm-0" style={{color:"white"}} type="submit">Search</button>
     </div>
-   {!loggedin && 
-    <Link to="/" style={{
-        textDecoration:'none',
-        color:'white'
-      }}>
-        <button className="  btn btn-primary my-2 my-sm-0 mx-2" style={{color:"white"}}type="submit">Login</button>
-      
-        </Link>
-      
-    
-        
-      
-  } 
-    {loggedin && <button className="  btn btn-primary my-2 my-sm-0 mx-2" style={{color:"white"}}type="submit" onClick={()=>{logout()}}>
-   
-      Logout
-      
-      
-    
-        
-      
-    </button> }
-  
+    {!loggedin && 
+      <Link to="/" style={{textDecoration:'none', color:'white'}}>
+        <button className="btn btn-primary my-2 my-sm-0 mx-2" style={{color:"white"}} type="submit" data-toggle="collapse" data-target=".navbar-collapse.show">Login</button>
+      </Link>
+    } 
+    {loggedin && 
+      <button className="btn btn-primary my-2 my-sm-0 mx-2" style={{color:"white"}} type="submit" onClick={()=>{logout()}} data-toggle="collapse" data-target=".navbar-collapse.show">Logout</button>
+    }
   </div>
 </nav>
     
